3ba47cfbc2b386ba3f8d93440c9a1edfc2e9ef33
progquartz <progquartz@gmail.com>
2025-01-15 20:35:20 +0900

    [feat] 09 - CharacterUI Minimal
    - Character UI Minimal
    - HpBar / Text

--- Assets/Scripts/Common/UI/HealthBarUI.cs
+++ Assets/Scripts/Common/UI/HealthBarUI.cs
@@ -1,10 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField] private BaseTarget target;
+    [SerializeField] private TMP_Text healthText;
     [SerializeField] private RectTransform healthBarRect;
     [SerializeField] private RectTransform healthBarOriginalRectSize;
+    
 
 
     private void Update()
@@ -14,9 +17,20 @@ public class HealthBarUI : MonoBehaviour
 
     private void UpdateHealthBar()
     {
+        UpdateHealthBarSize();
+        UpdateHealthBarText();
+    }
+
+    private void UpdateHealthBarSize()
+    {
         Vector2 originalSize = healthBarOriginalRectSize.sizeDelta;
         float hpRatio = (float)target.Hp / (float)target.maxHp;
 
-        healthBarRect.sizeDelta = new Vector2 (originalSize.x * hpRatio, originalSize.y);
+        healthBarRect.sizeDelta = new Vector2(originalSize.x * hpRatio, originalSize.y);
+    }
+
+    private void UpdateHealthBarText()
+    {
+        healthText.text = target.Hp + "/" + target.maxHp;
     }
 }
