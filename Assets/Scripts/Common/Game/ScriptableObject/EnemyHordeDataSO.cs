d843e5e49eada62c4836111c98948e6f942bf34e
progquartz <progquartz@gmail.com>
2025-01-23 12:13:45 +0900

    [feat] 21 - EnemyHordeDataSO
    - EnemyHordeDataSO

---
+++ Assets/Scripts/Common/Game/ScriptableObject/EnemyHordeDataSO.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public enum StageType
+{
+    Stage1Normal,
+    Stage1Elite,
+    Stage1Boss,
+
+    Stage2Normal,
+    Stage2Elite,
+    Stage2Boss,
+}
+
+[CreateAssetMenu(fileName = "EnemyHordeDataSO", menuName = "Scriptable Objects/EnemyHordeDataSO")]
+public class EnemyHordeDataSO : ScriptableObject
+{
+    [Header("이 호드가 등장할 스테이지 타입")]
+    public StageType stageType;
+
+    [Header("이 호드가 배치될 때 함께 등장할 적 목록")] 
+    public List<EnemyDataSO> enemyList;
+
+    // 스폰 비중 (필요할까?)
+    public float spawnWeight;
+    // public string hordeDescription;
+}
